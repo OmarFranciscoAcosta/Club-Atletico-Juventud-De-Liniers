b81a0d6e5d01e39a3a2dd917703a5132e6adf45f
Omar <omar.acosta.francisco@gmail.com>
2023-10-16 19:25:28 -0300

    updates

--- CAJL/aplications/payments/apps.py
+++ CAJL/aplications/payments/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class PaymentsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'CAJL.aplications.prices'
+    name = 'CAJL.aplications.payments'
--- CAJL/aplications/vouchers/admin.py
+++ CAJL/aplications/vouchers/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
-
+from .models import vouchers
 # Register your models here.
+class vouchersAdmin (admin.ModelAdmin):
+    list_display = (
+        'id',
+        'fecha_comprobante',
+        'observacion',
+        'estado',
+        'tipo_pago',
+        'nombre_quien_paga',
+    )
+    search_fields = ['fecha_comprobante','estado','tipo_pago']
+    list_filter = ['estado','tipo_pago']
+
+admin.site.register(vouchers,vouchersAdmin)
