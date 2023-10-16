a28b58de35601d10f99ee6d97238e3ff1f583a2f
Omar <omar.acosta.francisco@gmail.com>
2023-10-16 19:43:35 -0300

    updates

--- CAJL/aplications/payments/admin.py
+++ CAJL/aplications/payments/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import payments
 
 # Register your models here.
+class paymentsAdmin (admin.ModelAdmin):
+    list_display = (
+        'id',
+        'fecha_pago',
+        'monto_pago',
+    )
+    search_fields = ['id','fecha_pago','monto_pago']
+    list_filter = ['fecha_pago']
+
+admin.site.register(payments,paymentsAdmin)
--- CAJL/aplications/payments/models.py
+++ CAJL/aplications/payments/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from CAJL.aplications.vouchers.models import vouchers
 
 # Create your models here.
+class payments (models.Model):
+    comprobante = models.ForeignKey (vouchers,on_delete=models.CASCADE, blank=True)
+    fecha_pago = models.DateField ('Fecha del pago',blank=True)
+    monto_pago = models.DecimalField('Monto del pago',max_digits=10, decimal_places=2,blank=True, null=True)
+    
+    
+    
+    class Meta:
+        verbose_name = 'Pagos'
+        verbose_name_plural = 'Pagos del club'
+        ordering = ['fecha_pago']
+
+def __str__(self):
+        return f'{self.id} - {self.fecha_pago} - {self.monto_pago}'
