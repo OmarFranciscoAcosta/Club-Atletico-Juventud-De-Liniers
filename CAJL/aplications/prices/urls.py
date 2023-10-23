55e067d78f65956afd1823652f255f525fa3b3fd
Omar <omar.acosta.francisco@gmail.com>
2023-10-23 00:25:19 -0300

    updates

--- CAJL/aplications/prices/urls.py
+++ CAJL/aplications/prices/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path
-from .views import prices_list
+from .views import prices_list, carga_datos
 urlpatterns = [
     path('prices_list/', prices_list, name='prices_list'),
+    path('prices_add/', carga_datos, name='prices_add'),
 
 ]
--- CAJL/aplications/prices/views.py
+++ CAJL/aplications/prices/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from .forms import PricesForm
 from .models import prices
 # Create your views here.
 
@@ -13,3 +14,18 @@ def prices_list(request):
 
     return render(request, 'prices/prices_list.html', context)
 
+#Vista para generar la carga de un precio de la actividad:
+def carga_datos(request):
+    if request.method == 'POST':
+        form = PricesForm(request.POST)
+        if form.is_valid():
+            form.save()  # Guarda los datos en la base de datos
+
+            # Devuelve una respuesta JSON para indicar éxito
+            return JsonResponse({'success': True})
+
+    # Resto del código como lo tenías originalmente
+    else:
+        form = PricesForm()
+
+    return render(request, 'prices/carga_precio.html', {'form': form})
