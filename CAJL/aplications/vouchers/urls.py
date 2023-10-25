153690d10772b2036dcaeb2d1a85cbc1e13229d0
Omar <omar.acosta.francisco@gmail.com>
2023-10-25 03:05:06 -0300

    updates

--- CAJL/aplications/vouchers/urls.py
+++ CAJL/aplications/vouchers/urls.py
@@ -14,15 +14,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('',include('CAJL.aplications.loginregister.urls')),
-    path('accounts/',include('django.contrib.auth.urls')),
-    path('',include('CAJL.aplications.partners.urls')),
-    path('',include('CAJL.aplications.activities.urls')),
-    path('',include('CAJL.aplications.prices.urls')),
-    path('',include('CAJL.aplications.vouchers.urls')),
+    path('comprobantes/', views.comprobantes_list, name='comprobantes'),
 ]
--- CAJL/aplications/vouchers/views.py
+++ CAJL/aplications/vouchers/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from .models import vouchers
 # Create your views here.
+
+#VISTA PARA GENERAR UNA DATATABLE.
+def comprobantes_list(request):
+    comprobantes = vouchers.objects.all()
+    return render(request, 'vouchers/comprobantes.html', {'comprobantes': comprobantes})
