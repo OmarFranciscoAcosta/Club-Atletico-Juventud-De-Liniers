09a044d91e265907a4be6468e65bc02b4daf5536
Omar <omar.acosta.francisco@gmail.com>
2023-10-26 02:14:16 -0300

    updates

--- CAJL/aplications/payments/urls.py
+++ CAJL/aplications/payments/urls.py
@@ -14,15 +14,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from . import views
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('',include('CAJL.aplications.loginregister.urls')),
-    path('accounts/',include('django.contrib.auth.urls')),
-    path('',include('CAJL.aplications.partners.urls')),
-    path('',include('CAJL.aplications.activities.urls')),
-    path('',include('CAJL.aplications.prices.urls')),
-    path('',include('CAJL.aplications.payments.urls')),
+    path('pagos/', views.payments_list, name='payments_list'),
+    # Agrega más rutas si es necesario
 ]
--- CAJL/aplications/payments/views.py
+++ CAJL/aplications/payments/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import payments
 
 # Create your views here.
+
+
+#RENDER DATOS PARA DATATABLE
+def payments_list(request):
+    # Recupera los pagos desde la base de datos
+    payments_data = payments.objects.all()
+
+    # Pasa los datos a la plantilla
+    context = {'payments_data': payments_data}
+    return render(request, 'payments/payments_list.html', context)
