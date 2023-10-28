e14b798390d68e1cf8c9a2d38a529771da198f55
Omar <omar.acosta.francisco@gmail.com>
2023-10-28 05:37:39 -0300

    updates

--- CAJL/aplications/partners/views.py
+++ CAJL/aplications/partners/views.py
@@ -46,9 +46,6 @@ def agregar_socio(request):
             socio = form.save(commit=False)  # Guarda la instancia sin hacer commit
             socio.user = request.user  # Asigna el usuario actual
             socio.save()  # Ahora puedes guardar la instancia con el usuario asignado
-            
-            messages.success(request, 'El socio ha sido agregado correctamente.')
-            
             return redirect('socios')  # Redirige a la página de lista de socios (ajusta la URL según tu configuración)
         
     else:
