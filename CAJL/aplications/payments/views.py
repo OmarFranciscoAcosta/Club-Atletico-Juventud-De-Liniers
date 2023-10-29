f75608b97a1615d1380e0133ab135c70890aeb51
Omar <omar.acosta.francisco@gmail.com>
2023-10-29 14:03:13 -0300

    updates

--- CAJL/aplications/payments/views.py
+++ CAJL/aplications/payments/views.py
@@ -19,18 +19,25 @@ def payments_list(request):
 #RENDER DATOS PARA CARGAR COMPROBANTE
 def carga_comprobante(request):
     if request.method == 'POST':
+        # Si se envió un formulario (POST), procesa los datos y guárdalos en la base de datos.
         form = PaymentsForm(request.POST)
         if form.is_valid():
-            form.save()  # Guarda los datos en la base de datos
-
-            # Devuelve una respuesta JSON para indicar éxito
-            return JsonResponse({'success': True})
-
-    # Resto del código como lo tenías originalmente
+            comprobante = form.save(commit=False)  # Guarda la instancia sin hacer commit
+            comprobante.user = request.user  # Asigna el usuario actual
+            comprobante.save()  # Ahora puedes guardar la instancia con el usuario asignado
+            return redirect('payments_list')  # Redirige a la página de lista de socios (ajusta la URL según tu configuración)
+        
     else:
+        # Si no se envió un formulario (GET), muestra el formulario para agregar un socio.
         form = PaymentsForm()
-
+    
     return render(request, 'payments/carga_comprobante.html', {'form': form})
+
+
+
+
+
+
 
 #RENDER PARA DETALLES DEL COMPROBANTE
 def detalles_comprobante(request, payments_id):
