009bbe00a37aab6b950000a1ffe27da818c02445
Omar <omar.acosta.francisco@gmail.com>
2023-11-01 23:35:12 -0300

    updates

--- CAJL/aplications/loginregister/views.py
+++ CAJL/aplications/loginregister/views.py
@@ -33,8 +33,6 @@ def register(request):
         if user_creation_form.is_valid():
             user_creation_form.save()
             
-            user = authenticate(username=user_creation_form.cleaned_data['username'], password=user_creation_form.cleaned_data['password1'])
-            login(request, user)
             
             return redirect('home')
     
