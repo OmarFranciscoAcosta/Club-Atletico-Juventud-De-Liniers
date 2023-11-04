60d0d202b267eb6ef59942d86b71875627774a14
Omar <omar.acosta.francisco@gmail.com>
2023-11-03 22:23:21 -0300

    updates

--- CAJL/aplications/loginregister/forms.py
+++ CAJL/aplications/loginregister/forms.py
@@ -2,8 +2,16 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
-class CustomUserCreationForm (UserCreationForm):
-    pass
+class CustomUserCreationForm(UserCreationForm):
+    def __init__(self, *args, **kwargs):
+        super(CustomUserCreationForm, self).__init__(*args, **kwargs)
+
+        # Personaliza el campo de ayuda para los requisitos de contraseña
+        self.fields['password1'].help_text = (
+            "La contraseña debe contener al menos 8 caracteres, incluyendo letras mayúsculas y minúsculas, "
+            "números y caracteres especiales como @, #, $."
+        )
+
     class Meta:
         model = User
         fields = ['username', 'first_name', 'last_name', 'email', 'password1', 'password2']
