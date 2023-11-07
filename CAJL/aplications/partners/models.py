fcaaa83eae9ff2a4e8e82ce2bc7ed1f33989321b
Omar <omar.acosta.francisco@gmail.com>
2023-11-07 00:40:03 -0300

    updates

--- CAJL/aplications/partners/models.py
+++ CAJL/aplications/partners/models.py
@@ -3,13 +3,17 @@ from django.db import models
 from CAJL.aplications.location.models import location
 from CAJL.aplications.activities.models import activities
 from django.contrib.auth.models import User
+from django.core.validators import RegexValidator
 # Create your models here.
 
 
-
+letters_only_validator = RegexValidator(
+    regex=r'^[a-zA-Z\s]*$',
+    message="Este campo solo puede contener letras y espacios en blanco."
+)
 class partners (models.Model):
     
-    nombre_completo = models.CharField ('Nombre completo',max_length=50)
+    nombre_completo = models.CharField('Nombre completo', max_length=50, validators=[letters_only_validator])
     dni = models.IntegerField('DNI')
     fecha_nacimiento = models.DateField('Fecha de nacimiento')
     direccion = models.CharField('Dirección', max_length=50)
@@ -20,14 +24,14 @@ class partners (models.Model):
     
     distrito = models.CharField('Distrito',max_length=2, choices=DISTRICT_CHOICES)
     localidad = models.ForeignKey(location, on_delete=models.CASCADE)
-    telefono1 = models.CharField('Teléfono 1',max_length=15,blank=True)
+    telefono1 = models.CharField('Teléfono 1',max_length=15)
     telefono2 = models.CharField('Teléfono 2',max_length=15,blank=True)
     actividades = models.ManyToManyField(activities)
     correo = models.EmailField('Email',max_length=40,blank=True)
     descripcion = models.CharField('Descripción',max_length=50, blank=True)
     apto_fisico = models.BooleanField('Apto físico',default=False)
-    fecha_vencimiento_apto_fisico = models.DateField ('Fecha de emisión del apto físico',null=True)
-    fecha_vencimiento_fichaje = models.DateField ('Fecha de emisión del fichaje',null=True)
+    fecha_vencimiento_apto_fisico = models.DateField ('Fecha de emisión del apto físico',blank=True, null=True)
+    fecha_vencimiento_fichaje = models.DateField ('Fecha de emisión del fichaje',blank=True, null=True)
     compite = models.BooleanField('Fichado',default=False)
     socio_activo = models.BooleanField('Socio activo',default=False)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
