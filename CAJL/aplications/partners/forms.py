5ea3760e6e3cf9c1da50ecf21142c50b158a226f
Omar <omar.acosta.francisco@gmail.com>
2023-11-09 02:54:49 -0300

    updates

--- CAJL/aplications/partners/forms.py
+++ CAJL/aplications/partners/forms.py
@@ -1,9 +1,16 @@
 from django import forms
-from .models import partners
+from .models import partners, activities
 
 class PartnerForm(forms.ModelForm):
-    
+    actividades = forms.ModelMultipleChoiceField(
+        queryset=activities.objects.all(),
+        widget=forms.CheckboxSelectMultiple,
+        help_text=f'<a href="/agregar_actividad/" target="_blank">Agregar actividad</a>'   
+    )
     
     class Meta:
         model = partners
         exclude = ['user']
+        
+        
+        
