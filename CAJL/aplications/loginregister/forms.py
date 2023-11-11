9d7630f3c41572a4d156e22a8843fd0a2477835f
Omar <omar.acosta.francisco@gmail.com>
2023-11-11 02:25:10 -0300

    updates

--- CAJL/aplications/loginregister/forms.py
+++ CAJL/aplications/loginregister/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.contrib.auth.forms import AuthenticationForm
 
 class CustomUserCreationForm(UserCreationForm):
     def __init__(self, *args, **kwargs):
@@ -19,3 +20,4 @@ class UserDetailsForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['username', 'first_name', 'last_name', 'email', 'is_active']
+        
