26594017cbb1b9bb72a87a14ca6b4299db3f9709
Omar <omar.acosta.francisco@gmail.com>
2023-11-12 19:55:13 -0300

    updates

--- CAJL/aplications/loginregister/signals.py
+++ CAJL/aplications/loginregister/signals.py
@@ -2,7 +2,7 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.contrib.auth.models import User,Group
 from CAJL.aplications.changelog.models import ChangeLog
-
+from .translations import MODEL_TRANSLATIONS
 
 signal_connected = False  # Variable para rastrear si la señal está conectada
 
@@ -13,8 +13,11 @@ def user_created(sender, instance, created, **kwargs):
     if not signal_connected:
         if created:
             # Registro en el changelog
+            
+            model_name = MODEL_TRANSLATIONS.get(sender.__name__, sender.__name__)
+            
             description = f"Se creó el usuario: {instance.username}"
-            ChangeLog.objects.create(model_name='User', user=instance, description=description)
+            ChangeLog.objects.create(model_name=sender.__name__, user=instance, description=description)
 
             # Asignación automática al grupo
             group_name = "Administrativo"  # Reemplaza con el nombre de tu grupo
---
+++ CAJL/aplications/loginregister/translations.py
@@ -0,0 +1,8 @@
+MODEL_TRANSLATIONS = {
+    'partners': 'Socios',
+    'activities': 'Actividades',
+    'prices': 'Precios',
+    'payments': 'Pagos',
+    'User': 'Usuarios',
+    # Agrega más modelos según sea necesario
+}
