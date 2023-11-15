3614512dfce0721d817db98df29277b67a4dd09e
Omar <omar.acosta.francisco@gmail.com>
2023-11-15 20:28:37 -0300

    updates

--- CAJL/aplications/loginregister/signals.py
+++ CAJL/aplications/loginregister/signals.py
@@ -4,27 +4,18 @@ from django.contrib.auth.models import User,Group
 from CAJL.aplications.changelog.models import ChangeLog
 from .translations import MODEL_TRANSLATIONS
 
-signal_connected = False  # Variable para rastrear si la señal está conectada
-
-
-
-
-@receiver(post_save, sender=User, dispatch_uid="unique_user_created")
 def user_created(sender, instance, created, **kwargs):
-    global signal_connected  # Usa la variable global
+    if created:
+        # Registro en el changelog
+        model_name = MODEL_TRANSLATIONS.get(sender.__name__, sender.__name__)
+        description = f"Se creó el usuario: {instance.username}"
+        ChangeLog.objects.create(model_name=model_name, user=instance, description=description)
 
-    if not signal_connected:
-        if created:
-            # Registro en el changelog
-            
-            model_name = MODEL_TRANSLATIONS.get(sender.__name__, sender.__name__)
-            
-            description = f"Se creó el usuario: {instance.username}"
-            ChangeLog.objects.create(model_name=model_name, user=instance, description=description)
-
-            # Asignación automática al grupo
-            group_name = "Administrativo"  # Reemplaza con el nombre de tu grupo
-            group, created = Group.objects.get_or_create(name=group_name)
-            instance.groups.add(group)
-
-        signal_connected = True  # Establece la variable a True después de ejecutar la señal
+        # Asignación automática al grupo
+        group_name = "Administrativo"  # Reemplaza con el nombre de tu grupo
+        try:
+            group = Group.objects.get(name=group_name)
+            if not instance.groups.filter(name=group_name).exists():
+                instance.groups.add(group)
+        except Group.DoesNotExist:
+            pass  # Maneja la lógica si el grupo no existe
