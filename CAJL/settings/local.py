1966763a74ba228443a57bb605c1d6b118be11af
Omar <omar.acosta.francisco@gmail.com>
2023-11-24 00:01:28 -0300

    updates local.py

--- CAJL/settings/local.py
+++ CAJL/settings/local.py
@@ -8,17 +8,31 @@ ALLOWED_HOSTS = []
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'CAJL$club-atletico-juventud-de-liniersdb',
+        'USER': 'CAJL',
+        'PASSWORD': 'clubatleticojuventuddeliniers',
+        'HOST': 'CAJL.mysql.pythonanywhere-services.com',
+        'PORT': '3306',
     }
 }
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+STATIC_ROOT = BASE_DIR / "static"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
